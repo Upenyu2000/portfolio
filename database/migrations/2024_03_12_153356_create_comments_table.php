bbf615b4b3e285b94d089559aa5f0ad482cc0856
Upenyu <upshlangabeza@gmail.com>
2024-03-12 15:56:07 +0000

    added attributes name and email for both post and comment

--- database/migrations/2024_03_12_153356_create_comments_table.php
+++ database/migrations/2024_03_12_153356_create_comments_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
             $table->timestamps();
         });
     }
