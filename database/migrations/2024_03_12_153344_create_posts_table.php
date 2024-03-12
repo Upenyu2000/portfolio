dda5a5e36c15015ae07e357111b9033b15468527
Upenyu <upshlangabeza@gmail.com>
2024-03-12 17:48:00 +0000

    made seeder for post and comment

--- database/migrations/2024_03_12_153344_create_posts_table.php
+++ database/migrations/2024_03_12_153344_create_posts_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email');
+            $table->string('name'); 
+            $table->string('email')->unique();
             $table->timestamps();
         });
     }
